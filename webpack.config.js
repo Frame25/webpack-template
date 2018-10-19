3c5c6d90e3cfedcad59f264786c0a761b9cf0f02
pavel_k <pavel.k.bz@gmail.com>
2018-10-19 12:50:24 +0300

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
-const path = require('path')
-const HtmlPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const {
+  path = require('path'),
+  HtmlPlugin = require('html-webpack-plugin'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin')
+} = {}
 
-const jsConfig = require('./webpack/js.js')
-const cssConfig = require('./webpack/css.js')
-const sassConfig = require('./webpack/sass.js')
-const vueConfig = require('./webpack/vue.js')
-const pugConfig = require('./webpack/pug.js')
-const imgConfig = require('./webpack/img.js')
-const fontsConfig = require('./webpack/fonts.js')
+const {
+  jsConfig = require('./webpack/js.js'),
+  cssConfig = require('./webpack/css.js'),
+  sassConfig = require('./webpack/sass.js'),
+  vueConfig = require('./webpack/vue.js'),
+  pugConfig = require('./webpack/pug.js'),
+  imgConfig = require('./webpack/img.js'),
+  fontsConfig = require('./webpack/fonts.js')
+} = {}
 
 const extractSass = new ExtractTextPlugin({
   filename: 'css/[name].css'
 })
 
-module.exports = {
+const jsBundle = {
   entry: {
     index: './src/pages/index/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'js/[name].js'
-  },
+  }
+}
+
+const configs = {
   module: {
     rules: [ jsConfig(), cssConfig(), sassConfig(), vueConfig(), pugConfig(), fontsConfig(), imgConfig() ]
-  },
+  }
+}
+
+const plugins = {
   plugins: [
     new HtmlPlugin({
       title: 'index',
@@ -33,11 +43,40 @@ module.exports = {
       chunks: ['index']
     }),
     extractSass
-  ],
+  ]
+}
+
+const vueResolve = {
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
+  }
+}
+
+const build = {
+  ...jsBundle,
+  ...configs,
+  ...plugins,
+  ...vueResolve
+}
+
+const dev = {
+  ...build,
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 9000
+  }
+}
+
+module.exports = env => {
+  if (env === 'prod') {
+    return build
+  } else if (env === 'dev') {
+    return dev
+  } else {
+    return dev
   }
 }
