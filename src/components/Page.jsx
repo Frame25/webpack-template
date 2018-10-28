4e73f726d52d4351ce45feff847ee3dc89f449ba
pavel_k <pavel.k.bz@gmail.com>
2018-10-28 15:49:36 +0300

    input

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
 class Page extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      title: 'This is React Component'
+    }
+
+    this.setNewTitle = this.setNewTitle.bind(this)
+  }
+
+  setNewTitle (event) {
+    this.setState({
+      title: event.target.value || 'This is React Component'
+    })
+  }
+
   render () {
     return (
       <div>
-        <h1>This is React Component</h1>
+        <h1>{ this.state.title }</h1>
         <div className="desc">{this.props.description}</div>
+
+        <label className="test-input">
+          <h2>Type new title</h2>
+          <input type="text" onInput={this.setNewTitle} />
+        </label>
       </div>
     )
   }
