db1dfa406ca371b10a0f0c3514b1b5a56a4b991f
Pavel Kalchenko <pavel.k.bz@gmail.com>
2019-02-26 12:05:09 +0300

    new config

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # webpack-template
+I made it for myself, but if you want...
+
 This is a simple webpack project template. To fast init.
 
 If one wants to start new project and don`t want to spend time for writing config, 
@@ -74,4 +76,9 @@ git clone -b vue https://github.com/Frame25/webpack-template.git <your_project_n
 to call:
 ```shell
 git clone -b react https://github.com/Frame25/webpack-template.git <your_project_name>
+```
+4. The very simple project with React & React-Pug
+to call:
+```shell
+git clone -b react-pug https://github.com/Frame25/webpack-template.git <your_project_name>
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,176 @@
-const {
-  version = '1.0.0',
-  path = require('path'),
-  HtmlPlugin = require('html-webpack-plugin'),
-  MiniCssExtractPlugin = require('mini-css-extract-plugin')
-} = {}
-
-const {
-  jsConfig = require('./webpack-modules/js.js'),
-  stylesConfig = require('./webpack-modules/styles.js'),
-  vueConfig = require('./webpack-modules/vue.js'),
-  imgConfig = require('./webpack-modules/img.js'),
-  pugConfig = require('./webpack-modules/pug.js'),
-  fontsConfig = require('./webpack-modules/fonts.js'),
-  CopyWebpackPlugin = require('copy-webpack-plugin')
-} = {}
-
-const extractCss = new MiniCssExtractPlugin({
-  filename: 'css/[name].css?' + version
-})
-
-const jsBundle = {
-  entry: {
-    index: './src/pages/index/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'js/[name].js?' + version
-  }
-}
-
-const configs = {
-  module: {
-    rules: [ jsConfig(), stylesConfig(), vueConfig(), pugConfig(), fontsConfig(), imgConfig() ]
-  }
-}
-
-const plugins = {
-  plugins: [
-    new HtmlPlugin({
-      title: 'index',
-      filename: 'index.html',
-      template: './src/pages/index/index.html',
-      chunks: ['index']
-    }),
-    extractCss,
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, 'static/**/*'),
-        to: 'build'
-      }
-    ])
-  ]
-}
-
-const resolve = {
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.jsx', '.vue', '.json']
-  }
-}
-
-const common = {
-  ...jsBundle,
-  ...configs,
-  ...plugins,
-  ...resolve
-}
-
-const build = {
-  ...common
-}
-
-const dev = {
-  ...common,
-  devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    compress: true,
-    port: 9000
-  }
-}
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const sorting = require('postcss-sorting');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
-  if (env === 'prod') {
-    return build
-  } else if (env === 'dev') {
-    return dev
-  } else {
-    return dev
+  return {
+
+    //-----------------ENTRYPOINT---------------------//
+    entry: {
+      index: './src/pages/index/index.js',
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'js/[name].js?[hash]'
+    },
+
+    //-----------------MODULES---------------------//
+    module: {
+      rules: [
+        // JS CONFIG
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          exclude: file => (
+            /node_modules/.test(file)
+          ),
+          include: /src/,
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        // STYLES CONFIG
+        {
+          test: /\.sass$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                sourceMap: true,
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('cssnano')({
+                    preset: 'default'
+                  }),
+                  sorting(),
+                  autoprefixer({
+                    browsers: ['ie >= 8', 'last 4 version']
+                  })
+                ],
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                indentedSyntax: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                sourceMap: true,
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('cssnano')({
+                    preset: 'default'
+                  }),
+                  sorting(),
+                  autoprefixer({
+                    browsers: ['ie >= 8', 'last 4 version']
+                  })
+                ],
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+            }
+          ]
+        },
+        // PUG CONFIG
+        {
+          test: /\.pug$/,
+          use: [
+            {
+              loader: 'pug-loader',
+              options: {
+                pretty: env === 'development'
+              }
+            }
+          ],
+        },
+        // FONTS CONFIG
+        {
+          test: /\.(woff|woff2|eot|ttf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                // limit: '10000',
+                name: '[name].[ext]',
+                outputPath: 'fonts/',
+                publicPath: '../fonts/'
+              }
+            }
+          ]
+        },
+        // IMAGES CONFIG
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'img/[name].[ext]?[hash]'
+          }
+        }
+      ]
+    },
+
+    //-----------------PLUGINS---------------------//
+    plugins: [
+      new HtmlPlugin({
+        title: 'index',
+        filename: 'index.html',
+        template: './src/pages/index/index.html',
+        chunks: ['index']
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+      }),
+      // new CopyWebpackPlugin([
+      //   {
+      //     from: path.join(__dirname, 'static/img'),
+      //     to: 'img'
+      //   }
+      // ]),
+    ],
+
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.json']
+    },
+
+    //-----------------MODULES---------------------//
+    devServer: (env => {
+      return env === 'development' ? {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 9000
+      } : {}
+    })(env)
   }
 }
