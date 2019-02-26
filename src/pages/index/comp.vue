e4be54cd4ce455fdea86594d314aa06e5123012e
Pavel Kalchenko <pavel.k.bz@gmail.com>
2019-02-26 11:35:24 +0300

    new webpack config

---
+++ src/pages/index/comp.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .huy {{innerText}}
+</template>
+<script>
+export default {
+  name: "Huy",
+  props: ['text'],
+  computed: {
+    innerText () {
+      return this.text || 'default text'
+    }
+  }
+}
+</script>
+<style lang="sass">
+  .huy
+    color: red
+</style>
+
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,5 @@
 import './index.sass'
+// import comp from './comp.vue'
+// import Vue from 'vue'
+
+// document.getElementById('comp').appendChild((new (Vue.extend(comp))).$mount().$el)
