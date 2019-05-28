e3e08d273a51ed6ac7798da93ed08bc9fa962365
Pavel Kalchenko <pavel.k.bz@gmail.com>
2019-05-28 17:11:01 +0300

    add init script

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var exec = require('child_process').exec;
 ['.git', '.vscode', 'yarn-error.log', 'yarn.lock', 'init.js'].forEach(function(file){
   fs.unlink(file, function(err) { if (err) console.log('can not remove ' + file) });
 });
@@ -8,7 +9,7 @@ p = JSON.parse(p);
 delete p.scripts.init;
 fs.writeFileSync('package.json', JSON.stringify(p, null, 2), 'utf-8');
 
-child_process.exec('npm i');
-child_process.exec('git init');
+exec('npm i');
+exec('git init');
 
 console.log('Enjoy coding!')
